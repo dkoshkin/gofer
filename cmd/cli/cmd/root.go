8ab37ba8ccc9427e6656ac9481b167e17d19be5e
dkoshkin <dimitri.koshkin@gmail.com>
2018-08-13 21:48:54 -0400

    Remove initConfig() func

--- cmd/cli/cmd/root.go
+++ cmd/cli/cmd/root.go
@@ -16,12 +16,9 @@ package cmd
 
 import (
 	"encoding/json"
-	"fmt"
 	"os"
 
-	homedir "github.com/mitchellh/go-homedir"
 	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
 )
 
 const (
@@ -60,32 +57,5 @@ func Execute(version Version) {
 }
 
 func init() {
-	cobra.OnInitialize(initConfig)
 	rootCmd.PersistentFlags().StringVarP(&cfgFile, "config", "f", "./.gofer/config.yaml", "config file containing the list of dependencies")
-}
-
-// initConfig reads in config file and ENV variables if set.
-func initConfig() {
-	if cfgFile != "" {
-		// Use config file from the flag.
-		viper.SetConfigFile(cfgFile)
-	} else {
-		// Find home directory.
-		home, err := homedir.Dir()
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
-
-		// Search config in home directory with name ".gofer" (without extension).
-		viper.AddConfigPath(home)
-		viper.SetConfigName(".gofer")
-	}
-
-	viper.AutomaticEnv() // read in environment variables that match
-
-	// If a config file is found, read it in.
-	if err := viper.ReadInConfig(); err == nil {
-		//fmt.Println("Using config file:", viper.ConfigFileUsed())
-	}
 }
